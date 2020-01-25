cabd6968e7efc5e043c4629630e4366d8b286a9d
Jose Figueroa <Josefigueroalv@protonmail.com>
2020-01-25 13:19:21 -0800

    implemented Switch from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect,
+} from 'react-router-dom'
+
+const Home = () => <h1>Home</h1>
+const WillMatch = () => <h1>Matched!</h1>
+
+const NoMatch = ({location}) => (
+  <div>
+    <h3>No match for <code>{location.pathname}</code></h3>
+  </div>
+)
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <Router className="App">
+        <div>
+          <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/old-match'>Old Match, to be redirected</Link></li>
+            <li><Link to='/will-match'>Will Match</Link></li>
+            <li><Link to='/will-not-match'>Will Not Match</Link></li>
+            <li><Link to='/also/will/not/match'>Also will not match</Link></li>
+          </ul>
+
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Redirect from='/old-match' to='will-match' />
+            <Route path='/will-match' component={WillMatch} />
+            <Route component={NoMatch} />
+          </Switch>
+        </div>
+      </Router>
+    )
   }
 }
 
